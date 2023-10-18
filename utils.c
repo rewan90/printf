1bf9cd8ae8dc9be6e88a0efb34e8cd2283d201e0
Rewan Noufal <rewan.noufal.1998@gmail.com>
2023-10-18 03:04:35 +0300

    update

--- utils.c
+++ utils.c
@@ -8,10 +8,10 @@
  */
 int is_printable(char c)
 {
-  if (c >= 32 && c < 127)
-    return (1);
+	if (c >= 32 && c < 127)
+		return (1);
 
-  return (0);
+	return (0);
 }
 
 /**
@@ -19,22 +19,23 @@ int is_printable(char c)
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASSCI CODE.
+ *
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-  char map_to[] = "0123456789ABCDEF";
-  /* The hexa format code is always 2 digits long */
-  if (ascii_code < 0)
-    ascii_code *= -1;
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
 
-  buffer[i++] = '\\';
-  buffer[i++] = 'x';
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
-  buffer[i++] = map_to[ascii_code / 16];
-  buffer[i] = map_to[ascii_code % 16];
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
 
-  return (3);
+	return (3);
 }
 
 /**
@@ -45,10 +46,10 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  */
 int is_digit(char c)
 {
-  if (c >= '0' && c <= '9')
-    return (1);
+	if (c >= '0' && c <= '9')
+		return (1);
 
-  return (0);
+	return (0);
 }
 
 /**
@@ -60,12 +61,12 @@ int is_digit(char c)
  */
 long int convert_size_number(long int num, int size)
 {
-  if (size == S_LONG)
-    return (num);
-  else if (size == S_SHORT)
-    return ((short)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
 
-  return ((int)num);
+	return ((int)num);
 }
 
 /**
@@ -77,10 +78,10 @@ long int convert_size_number(long int num, int size)
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-  if (size == S_LONG)
-    return (num);
-  else if (size == S_SHORT)
-    return ((unsigned short)num);
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
 
-  return ((unsigned int)num);
+	return ((unsigned int)num);
 }
